a9dc806ded822af1436d7aeac93596b56dcc7c4d
Kelvin <26215587+zktuong@users.noreply.github.com>
2022-01-11 15:41:38 +0000

    add milo and updated scripts

---
+++ Bcells/scripts/DEG/glmmSeq_B_ASC_dividing-Copy1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#BSUB -G teichlab
+#BSUB -P team205
+#BSUB -J glmmSeq
+#BSUB -q normal
+#BSUB -o %J.out
+#BSUB -n 24
+#BSUB -R "select[mem>80000] rusage[mem=80000] span[hosts=1]" -M80000
+### ~~~ job script below ~~~ ###
+
+Rscript --vanilla glmmSeq_jobs2.R \
+       -i /lustre/scratch117/cellgen/team297/kt16/COVID_imperial_renal/h5ad/df.fil3_gex_bcells_vdj_sce_B_ASC_dividing.RDS \
+       -o /lustre/scratch117/cellgen/team297/kt16/COVID_imperial_renal/h5ad/glmmSeq_B_ASC_dividing2.RData \
+       -m 5 \
+       -n 24
+
---
+++ Bcells/scripts/DEG/glmmSeq_jobs2.R
@@ -0,0 +1,63 @@
+#!/usr/bin/env Rscript
+suppressPackageStartupMessages(library(SingleCellExperiment))
+suppressPackageStartupMessages(library(SummarizedExperiment))
+suppressPackageStartupMessages(library(variancePartition))
+suppressPackageStartupMessages(library(limma))
+suppressPackageStartupMessages(library(edgeR))
+suppressPackageStartupMessages(library(scran))
+suppressPackageStartupMessages(library(scater))
+suppressPackageStartupMessages(library(glmmSeq))
+suppressPackageStartupMessages(library(optparse))
+
+opt_list <- list(make_option(c("-i", "--in"), dest="IN", help="input sce .RDS file."),
+                 make_option(c("-o", "--out"), dest="OUT", help="output result .RData file."),
+                 make_option(c("-m", "--min"), dest="MIN", default= 5, help="minimum number of cells."),
+                 make_option(c("-n", "--ncpu"), dest="NCPU", default= 1, help="number of cores"))
+# Parse arguments
+opt <- parse_args(OptionParser(option_list=opt_list))
+
+sce <- readRDS(opt$IN)
+counts(sce) <- assays(sce)[['X']]
+sce$case_control <- factor(sce$case_control, levels = c('NEGATIVE', 'POSITIVE', 'RECOVERY'))
+
+# Remove samples with less than MIN
+nCells <- table(sce$sample_id)
+rmSamples <- names(nCells[nCells<opt$MIN])
+sce <- sce[,!sce$sample_id %in% rmSamples]
+# Summarize Counts
+smrzd <- aggregateAcrossCells(sce, id=as.character(colData(sce)[,c("sample_id")]))
+y <- DGEList(counts=counts(smrzd), samples=colData(smrzd))
+keep <- filterByExpr(y, group=y$samples$case_control, min.count=3, min.total.count=5)
+y <- y[keep,]
+# ensure factors are respected
+y$samples$case_control <- droplevels(y$samples$case_control)
+y$samples$case_control <- ordered(y$samples$case_control)
+# Estimate Dispersion
+disp  <- suppressMessages(setNames(edgeR::estimateDisp(y)$tagwise.dispersion, rownames(y)))
+# Norm
+sizeFactors <- calcNormFactors(y$counts)
+
+# run glmmseq
+results1 <- glmmSeq(~ case_control + sex + ethnicity + calc_age + (1|individual_id),
+                  id = "individual_id",
+                  countdata = y$counts,
+                  metadata = y$samples,
+                  dispersion = disp,
+                  sizeFactors = sizeFactors,
+                  removeDuplicatedMeasures = FALSE,
+                  designMatrix = model.matrix(~ case_control + sex + ethnicity + calc_age, data=y$samples),
+                  removeSingles=FALSE,
+                  progress=TRUE,
+                  cores = opt$NCPU)
+results2 <- glmmSeq(~ case_control + sex + ethnicity + calc_age + days_to_admission + (1|individual_id),
+                  id = "individual_id",
+                  countdata = y$counts,
+                  metadata = y$samples,
+                  dispersion = disp,
+                  sizeFactors = sizeFactors,
+                  designMatrix = model.matrix(~ case_control + sex + ethnicity + calc_age + days_to_admission, data=y$samples),
+                  removeDuplicatedMeasures = FALSE,
+                  removeSingles=FALSE,
+                  progress=TRUE,
+                  cores = opt$NCPU)
+save(results1, results2, file = opt$OUT)
